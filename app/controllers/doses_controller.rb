f37a23d13bc887c81dae82a60a1e2d5e227877e9
clementine contat <clementine.contat@gmail.com>
2019-02-21 14:52:12 +0000

    added doses controller and cocktails controller with actions and views

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: %i[new create]
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'cocktails#index'
-  get 'cocktails', to: 'cocktails#create'
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+  end
+  delete 'doses/:id', to: 'doses#destroy', as: 'dose'
 end
