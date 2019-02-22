7add727cc3a8b762688b71d2c5ee1a64f9499e0a
clementine contat <clementine.contat@gmail.com>
2019-02-22 12:01:33 +0000

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,8 @@ html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
 
 array_names = html_doc.search('h3').map do |element|
-  element.text.gsub(/\d+\.\s/, '').strip
+  element.text.strip
+  # element.next_element.text
 end
 
 array_p = html_doc.search('h3 + p').map do |element|
@@ -33,28 +34,35 @@ end
 
 array_p.unshift(img: 'https://static.vinepair.com/wp-content/uploads/2017/11/white-russian-inside.jpg',
                 p: 'Created in 1949 by a Belgian bartender named Gustave Tops, and popularized by the 1998 film “The Big Lebowski,” the White Russian continues to be a world-wide favorite. The White Russian combines heavy cream (or half and half), vodka, and coffee liqueur for a luxurious nightcap.')
-p array_p.count
-array_names.delete('Sidecar')
-p array_names[2..50]
-p array_names[2..50].count
+# p array_p
+# p array_p.count
+array_names.delete('30. Sidecar')
+array_names.delete('5. Manhattan')
+# p array_names[2..49]
+# p array_names[2..49].count
 
-# array_names[3..51].each_with_index do |name, i|
-#   image = if array_p[i][:img] == ''
-#             'https://images.pexels.com/photos/274192/pexels-photo-274192.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-#           else array_p[i][:img]
-#           end
-#   Cocktail.new(
-#     name: name,
-#     description: array_p[i][:p],
-#     image: image
-#   )
-# end
+array_names[2..49].each_with_index do |name, i|
+  image = if array_p[i][:img] == ''
+            'https://images.pexels.com/photos/274192/pexels-photo-274192.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+          else array_p[i][:img]
+          end
+  Cocktail.create(
+    name: name,
+    description: array_p[i][:p],
+    image: image
+  )
+end
 
-Cocktail.new(
-  name: 'Sidecar',
+Cocktail.create(
+  name: '30. Sidecar',
   description: 'Brandy, tragically underrepresented on this list, earns a well-deserved moment in the worldwide spotlight as the star of one of the world’s most-ordered cocktails. The Sidecar is a good place to start for those not familiar with the category-spanning spirit: the drink mixes brandy, lemon, and triple sec, making a tart, refreshing tipple.',
   image: 'https://static.vinepair.com/wp-content/uploads/2017/11/sidecar-inside.jpg'
 )
 
+Cocktail.create(
+  name: '5. Manhattan',
+  description: 'It’s hard to stray from the Manhattan, and the recent rise of rye whiskey makes it even more difficult. Spicy rye, sweet vermouth, and two dashes of Angostura, stirred, strained, and garnished with a brandied cherry can make you feel like a true class act.',
+  image: 'https://static.vinepair.com/wp-content/uploads/2017/11/manhattan-inside.jpg'
+)
 
 puts 'finished seeding cocktails'
